4b5039ed955dbd9ab374b3fe83517e8a847bb053
Karen Cristina <karen-kaah@hotmail.com>
2024-11-26 22:50:48 +0100

    date

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,4 +1,19 @@
+import dayjs from "dayjs"
+
 const form=document.querySelector("form")
+
+
+const selectedDate=document.getElementById("date")
+
+
+
+// Carrega a data atual e formata corretamente
+const today = dayjs().format("YYYY-MM-DD");
+selectedDate.value = today;
+
+// Define a data mínima
+selectedDate.min = today;
+
 
 form.onsubmit=(event)=>{
     event.preventDefault()
